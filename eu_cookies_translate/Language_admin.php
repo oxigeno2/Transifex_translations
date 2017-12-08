b056f94341330c72c7cb50cf45a3cd20b9b8233a
oxigeno2 <oxigen.rg@gmail.com>
2017-12-08 18:08:30 +0100

    Add eu cookies language

---
+++ eu_cookies_translate/Language_admin.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2017 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link https://github.com/oxigeno2/eu_cookies
+ */
+define("LAN_PLUG_EUC_STYLE", "Style");
+define("LAN_PLUG_EUC_TEXT", "Text for the banner");
+define("LAN_PLUG_EUC_POLICYLINK", "Link to the extended cookie policy");
+define("LAN_PLUG_EUC_BTN", "It is the text for the acceptance button.");
+define("LAN_PLUG_EUC_POSITION", "Locations banner");
+define("LAN_PLUG_EUC_OPENEFFECT", "It is the opening effect for Divas Cookies banner.");
+define("LAN_PLUG_EUC_OPENEFFECTDURATION", "It is the duration in msec for the opening effect");
+define("LAN_PLUG_EUC_OPENEFFECTEASING", "It is the easing function for the opening effect");
+define("LAN_PLUG_EUC_CLOSEEFFECT", "It is the closing effect");
+define("LAN_PLUG_EUC_CLOSEEFFECTDURATION", "It is the duration in msec for the closing effect");
+define("LAN_PLUG_EUC_CLOSEEFFECTEASING", "It is the easing function for the closing effect ");
+define("LAN_PLUG_EUC_TOP", "Top");
+define("LAN_PLUG_EUC_BOTTOM", "Bottom");
+define("LAN_PLUG_EUC_DARK", "Dark");
+define("LAN_PLUG_EUC_LIGHT", "Light");
+define("LAN_PLUG_EUC_FADE", "Fade");
+define("LAN_PLUG_EUC_SLIDE_DOWN", "Slide down");
+define("LAN_PLUG_EUC_SLIDE_UP", "Slide up");
+define("LAN_PLUG_EUC_SLIDE_LEFT", "Slide left");
+define("LAN_PLUG_EUC_SLIDE_RIGHT", "Slide right");
+define("LAN_PLUG_EUC_SWING", "Swing");
+define("LAN_PLUG_EUC_LINEAR", "Linear");
+define("LAN_PLUG_EUC_POLICYLINKURL", "URL to your cookie policy page");
+define("LAN_PLUG_EUC_ANIMATION", "Animation");
+define("LAN_PLUG_EUC_OTHER", "Other");
+define("LAN_PLUG_EUC_SCROLL", "Accept On Scroll");
+define("LAN_PLUG_EUC_TIME", "Cookie Duration (days)");
+define("LAN_PLUG_EUC_1", "Banner width");
+define("LAN_PLUG_EUC_2", "Banner background");
+define("LAN_PLUG_EUC_3", "Banner font color");
+define("LAN_PLUG_EUC_4", "Line height");
+define("LAN_PLUG_EUC_5", "Font size");
+define("LAN_PLUG_EUC_6", "Button background");
+define("LAN_PLUG_EUC_7", "Button background hover");
+define("LAN_PLUG_EUC_8", "Button font color");
+define("LAN_PLUG_EUC_9", "Button font size");
+// Help field
+define("LAN_PLUG_EUC_HELP_TEXT", "Explaining the use of cookies, what purpose,");
+define("LAN_PLUG_EUC_HELP_POLICYLINK", "Button description, link to the official site with explanation.");
+define("LAN_PLUG_EUC_HELP_BTN", "Description button to agree to the use of cookies.");
+define("LAN_PLUG_EUC_HELP_SCROLL", "Accept the conditions by scrolling");
+// Help administration
+define("LAN_PLUG_EUC_HELP_ADMIN_01", "Explanation");
+define("LAN_PLUG_EUC_HELP_ADMIN_02", "Cookies are short text files that a server places on a user's computer when viewing websites. Cookies collect information about user behavior on the Internet, serve for personalizing content, tracking, measuring, and identifying.");
+define("LAN_PLUG_EUC_HELP_ADMIN_03", "Who shall require consent to use cookies?");
+define("LAN_PLUG_EUC_HELP_ADMIN_04", "The user must be provided with clear, comprehensible and complete information on the processing of the data, in particular its purpose. It is not sufficient if the information is available to the user somewhere on websites where he would have to search for it himself. This information must be provided directly.");
+?>
---
+++ eu_cookies_translate/Language_global.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * EU cookies plugin for CMS e107 v2
+ *
+ * @author OxigenO2 (oxigen.rg@gmail.com)
+ * @copyright Copyright (C) 2017 OxigenO2 
+ * @license GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * @link https://github.com/oxigeno2/eu_cookies
+ */
+define("LAN_PLUG_EUC_NAME", "EU cookies");
+define("LAN_PLUG_EUC_SUMMARY",  "EU Cookie informs users that your site uses cookies."); 
+define("LAN_PLUG_EUC_DESCRIPTION", "EU Cookie informs users that your site uses cookies, plugin created with the help a script Divas Cookies.");
+
+?>
